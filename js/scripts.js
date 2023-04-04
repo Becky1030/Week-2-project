f185cb22dc92a7e7eacfc2b427dd7fd783fe937f
Becky1030 <112721395+Becky1030@users.noreply.github.com>
2023-04-03 20:14:08 -0400

    Updated background according to day and night time, and added a image slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,34 @@
 $(document).ready(function () {
   $(".switch").on("click", function () {
     $(".toggle-btn").toggleClass("active");
+    $("body").toggleClass("dark");
   });
+
+
+$('.next').on('click', function(){
+  var currentImg = $('.active');
+  var nextImg = currentImg.next();
+
+  if(nextImg.length){
+    currentImg.removeClass('active').css('z-index', -10);
+    nextImg.addClass('active').css('z-index', 10);
+
+  }
+
+
+});
+
+
+$('.prev').on('click', function(){
+  var currentImg = $('.active');
+  var prevImg = currentImg.prev();
+
+  if(prevImg.length){
+    currentImg.removeClass('active').css('z-index', -10);
+    prevImg.addClass('active').css('z-index', 10);
+
+  }
+
+
+});
 });
